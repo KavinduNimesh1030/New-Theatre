6319dfb016b78d47cee42af807bb32327de1a6ca
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-03-14 22:03:49 +0530

    added main manu option

--- src/Theatre.java
+++ src/Theatre.java
@@ -4,4 +4,23 @@ public class Theatre {
     int[] row1 = {};
     int[] row2 = {};
     int[] row3 = {};
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the New Theatre");
+
+        System.out.println("Please select an option:");
+        System.out.println("1) Buy a ticket");
+        System.out.println("2) Print seating area");
+        System.out.println("3) Cancel ticket ");
+        System.out.println("4) List available seats");
+        System.out.println("5) Save to file");
+        System.out.println("6) Load from file");
+        System.out.println("7) Print ticket information and total price");
+        System.out.println("8) Sort tickets by price");
+        System.out.println("0) Quit");
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println("Enter Option :");
+    }
 }
