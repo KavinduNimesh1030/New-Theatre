5bfc9fb8f5e44454373330fc4989ab9ac8b7fb16
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-03-15 17:22:45 +0530

    buy ticket method completed

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,8 +1,9 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Theatre {
-    static int[] row1 = {0,0,0,0,0,0,0,0,0,0,0,0};
+    static int[] row1 = {0,1,0,0,0,0,0,0,0,0,0,0};
     static int[] row2 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     static int[] row3 = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
@@ -44,20 +45,48 @@ public class Theatre {
     }
 
     public static void buyTicket(){
+
         System.out.println("Enter Row Number : ");
         String rowNumber = scanner.nextLine();
-        System.out.println(rowNumber);
+
         System.out.println("Enter Seat Number : ");
         int seatNumber = Integer.parseInt(scanner.nextLine());
-        System.out.println(seatNumber);
 
         //check which array matches for user input row number
         if(rowNumber.equals("1")){
+
             //check valid seat number
-            if(!(seatNumber <=15 && seatNumber>0)){
-                System.out.println("please Enter a valid seat Number..!");
-                
+            bookTicket(row1,seatNumber);
+
+        }else if(rowNumber.equals("2")){
+
+            bookTicket(row2,seatNumber);
+
+        }else if(rowNumber.equals("3")){
+
+            bookTicket(row3,seatNumber);
+        }else {
+            System.out.println("please Enter a valid Row Number..!");
+        }
+    }
+
+    //This method created for avoid boilerplate codes
+    public static void bookTicket(int[]row,int seatNumber){
+        
+        if(seatNumber <=15 && seatNumber>0){
+
+            System.out.println("data "+row[seatNumber-1]);
+            //check seat availability
+            if(row[seatNumber-1] == 0){
+                System.out.println("seat available");
+                row[seatNumber-1] = 1;
+                /*     System.out.println("Array"+Arrays.toString(row1));*/
+            }else {
+                System.out.println("Seat Not Available ..!");
             }
+
+        }else {
+            System.out.println("please Enter a valid seat Number..!");
         }
     }
 }
