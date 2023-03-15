07dce9c4ff9d520a6c1a3200e266c3eaac9e413e
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-03-15 21:09:45 +0530

    created print method in ticket class

--- src/Ticket.java
+++ src/Ticket.java
@@ -13,4 +13,12 @@ public class Ticket {
         this.price = price;
         this.person = person;
     }
+    public void print(){
+        System.out.println("Name :"+person.name);
+        System.out.println("Surname :"+person.surname);
+        System.out.println("Email :"+person.email);
+        System.out.println("Row :"+row);
+        System.out.println("Seat :"+seat);
+        System.out.println("Price :"+price);
+    }
 }
